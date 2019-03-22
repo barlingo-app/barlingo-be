5684e63cccf0c1964f26aadf881a06eac0db1030
angpomluz <angelpomaresus@gmail.com>
2019-03-22 12:14:48 +0100

    @angpomluz - #3 - Fixing conf issues

--- src/main/java/com/barlingo/backend/models/entities/Language.java
+++ src/main/java/com/barlingo/backend/models/entities/Language.java
@@ -1,6 +1,9 @@
 package com.barlingo.backend.models.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.URL;
@@ -10,6 +13,10 @@ import lombok.Data;
 @Entity
 @Data
 public class Language {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	
 	@NotBlank
 	private String name;
--- src/main/java/com/barlingo/backend/models/entities/UserDiscount.java
+++ src/main/java/com/barlingo/backend/models/entities/UserDiscount.java
@@ -1,6 +1,9 @@
 package com.barlingo.backend.models.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.Data;
 
@@ -8,4 +11,7 @@ import lombok.Data;
 @Entity
 public class UserDiscount {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 }
