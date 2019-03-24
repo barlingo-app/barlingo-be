07a8e83622b5a456170be25379cdc62e51d9cd83
fernando <fheraa@gmail.com>
2019-03-24 21:47:33 +0100

    @fhera - #5 - Fix db connection to innoDB and cleanCode in DomainEntity

--- src/main/java/com/barlingo/backend/models/entities/DomainEntity.java
+++ src/main/java/com/barlingo/backend/models/entities/DomainEntity.java
@@ -10,17 +10,13 @@ import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Version;
 
+import lombok.Data;
+
 @Entity
 @Access(AccessType.FIELD)
+@Data
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class DomainEntity {
-
-	/////////////////
-	// Constructor //
-	/////////////////
-	public DomainEntity() {
-		super();
-	}
 
 	////////////////
 	// Attributes //
@@ -31,59 +27,6 @@ public abstract class DomainEntity {
 	@Version
 	private int version;
 
-	public int getId() {
-		return this.id;
-	}
-
-	public void setId(final int id) {
-		this.id = id;
-	}
-
-	public int getVersion() {
-		return this.version;
-	}
-
-	public void setVersion(final int version) {
-		this.version = version;
-	}
-
-	@Override
-	public int hashCode() {
-		return this.getId();
-	}
-
-	@Override
-	public boolean equals(final Object other) {
-		boolean result;
-
-		if (this == other)
-			result = true;
-		else if (other == null)
-			result = false;
-		else if (other instanceof Integer)
-			result = (this.getId() == (Integer) other);
-		else if (!this.getClass().isInstance(other))
-			result = false;
-		else
-			result = (this.getId() == ((DomainEntity) other).getId());
-
-		return result;
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder result;
-
-		result = new StringBuilder();
-		result.append(this.getClass().getName());
-		result.append("{");
-		result.append("id=");
-		result.append(this.getId());
-		result.append(", version=");
-		result.append(this.getVersion());
-		result.append("}");
-
-		return result.toString();
-	}
+	
 
 }
