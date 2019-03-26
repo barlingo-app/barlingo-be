6e7aeac55c42f19aee1cc539275ff360cb009a6e
Jesús Rivas Jiménez <jesus.rivas.jmnez@gmail.com>
2019-03-26 23:38:27 +0100

    @jesusrivasjmnez - CI/CD system
    * Update scripts to upload build package to UAT/PRD server.

--- scripts/generate-db-update.sh
+++ scripts/generate-db-update.sh
@@ -14,8 +14,8 @@ RESOURCES_PATH=src/main/resources
 mv "$RESOURCES_PATH/import.sql" "$RESOURCES_PATH/import.sql.bck"
 rm "$RESOURCES_PATH/liquibase-diff-changeLog.xml"
 
-mvn liquibase:diff -Ddb.url=$TARGET_DB_URL -Ddb.username=$TARGET_DB_USER -Ddb.password=$TARGET_DB_PASSWORD -Ddb.refurl=$SOURCE_DB_URL -Ddb.refusername=$SOURCE_DB_USER -Ddb.refpassword=$SOURCE_DB_PASSWORD
-mvn liquibase:updateSQL -Ddb.url=$TARGET_DB_URL -Ddb.username=$TARGET_DB_USER -Ddb.password=$TARGET_DB_PASSWORD -Ddb.refurl=$SOURCE_DB_URL -Ddb.refusername=$SOURCE_DB_USER -Ddb.refpassword=$SOURCE_DB_PASSWORD -Ddb.env=$ENVIRONMENT -Ddb.version=$VERSION
+mvn liquibase:diff -Ddb.url=$TARGET_DB_URL -Ddb.username=$TARGET_DB_USER -Ddb.password=$TARGET_DB_PASSWORD -Ddb.refurl=$SOURCE_DB_URL -Ddb.refusername=$SOURCE_DB_USER -Ddb.refpassword=$SOURCE_DB_PASSWORD -Dmaven.test.skip=true
+mvn liquibase:updateSQL -Ddb.url=$TARGET_DB_URL -Ddb.username=$TARGET_DB_USER -Ddb.password=$TARGET_DB_PASSWORD -Ddb.refurl=$SOURCE_DB_URL -Ddb.refusername=$SOURCE_DB_USER -Ddb.refpassword=$SOURCE_DB_PASSWORD -Ddb.env=$ENVIRONMENT -Ddb.version=$VERSION -Dmaven.test.skip=true
 
 rm "$RESOURCES_PATH/liquibase-diff-changeLog.xml"
 mv "$RESOURCES_PATH/import.sql.bck" "$RESOURCES_PATH/import.sql"
