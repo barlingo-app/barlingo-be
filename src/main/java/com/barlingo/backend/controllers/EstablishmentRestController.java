ed861cdff8be85429b2ddf458336eb6edf301639
angpomluz <angelpomaresus@gmail.com>
2019-03-28 11:45:46 +0100

    @angpomluz - #7 - Add findById method to the controller

--- src/main/java/com/barlingo/backend/controllers/EstablishmentRestController.java
+++ src/main/java/com/barlingo/backend/controllers/EstablishmentRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,15 +16,18 @@ import com.barlingo.backend.models.services.EstablishmentServiceImpl;
 @RestController
 @RequestMapping("/establishments")
 public class EstablishmentRestController {
-	
+
 	@Autowired
 	private EstablishmentServiceImpl establishmentService;
-	
+
 	@GetMapping("/list")
-	public List<Establishment> findAllEstablishments(){
+	public List<Establishment> findEstablishments() {
 		return this.establishmentService.findAll();
 	}
-	
-	
+
+	@GetMapping("/{id}")
+	public Establishment show(@PathVariable int id) {
+		return this.establishmentService.findById(id);
+	}
 
 }
