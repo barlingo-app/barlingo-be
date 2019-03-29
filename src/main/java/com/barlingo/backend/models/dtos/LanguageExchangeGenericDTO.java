7d1314b290d5a2e54ad412c53185b11145839ce8
MIguel Angel <junior7395@hotmail.es>
2019-03-29 21:41:48 +0100

    @migmogcam - #32 - create language exchange

--- src/main/java/com/barlingo/backend/models/dtos/LanguageExchangeGenericDTO.java
+++ src/main/java/com/barlingo/backend/models/dtos/LanguageExchangeGenericDTO.java
@@ -3,15 +3,7 @@ package com.barlingo.backend.models.dtos;
 import java.util.Collection;
 import java.util.Date;
 
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-
-import com.barlingo.backend.models.entities.Establishment;
-import com.barlingo.backend.models.entities.ExchangeState;
 import com.barlingo.backend.models.entities.Language;
-import com.barlingo.backend.models.entities.User;
 
 import lombok.Data;
 
